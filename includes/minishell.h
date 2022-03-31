9e2328ac27e98d0019cd728d95c493de3d0d6ffa
Kiono78 <40069205+Kiono78@users.noreply.github.com>
2022-03-31 14:37:16 +0200

    update header file

---
+++ includes/minishell.h
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bterral <bterral@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 15:54:00 by bterral           #+#    #+#             */
+/*   Updated: 2022/03/31 14:35:08 by bterral          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef struct s_env t_env;
+
+typedef struct s_cmd
+{
+	char	**str;
+	int		token;
+	int		nb_of_arguments;
+	t_env	*head;
+}	t_cmd;
+
+typedef struct s_env
+{
+	char			*name;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
+/// to be classified ///
+
+int	is_whitespace(char c);
+
+/// BUILD IN FUNCTIONS not merged in master yet ///
+
+// int 	disable_option(char *str);
+// int 	is_build_in(t_cmd *cmd);
+// int		echo(t_cmd *cmd);
+// int 	cd(t_cmd *cmd);
+// int 	pwd(void);
+// int		unset(t_cmd *cmd);
+// int		env(t_cmd *cmd);
+// int		export(t_cmd *cmd);
+// int 	disable_option(char *str);
+// int 	ft_isalhpa_underscore(int c);
+// int 	ft_isalhpanum_underscore(int c);
+// int		is_var_valid(t_env **head, char *str);
+
+/// ENVIRONMENT VARIABLES ///
+
+t_env	*ft_env_new(char *name, char *value);
+void	ft_envadd_back(t_env **alst, t_env *new);
+t_env	*ft_envlast(t_env *lst);
+int		init_env_var(char **envp, t_env **head);
+t_env	*free_env_lst(t_env *head);
+t_env	*add_env_element(t_env **head, char *str);
+void	ft_envdel(t_cmd *cmd, char *name);
+void	free_env(t_env **head);
+
+
+#endif
