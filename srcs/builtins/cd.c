c65086373789381beaeed7307a57d0af683b4e4c
Benoit Terral <bterral@z4r11p3.42lyon.fr>
2022-05-10 17:02:20 +0200

    cd without argument

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: bterral <bterral@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 11:49:31 by bterral           #+#    #+#             */
-/*   Updated: 2022/05/10 15:10:57 by bterral          ###   ########.fr       */
+/*   Updated: 2022/05/10 16:59:13 by bterral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,35 @@ static int	free_return1(char *str)
 	return (1);
 }
 
+int	empty_cd(t_data *data)
+{
+	char	*current_dir;
+	char	*home_dir;
+
+	home_dir = my_getenv("HOME", &(data->head));
+	if (home_dir)
+	{
+		current_dir = getcwd(NULL, 0);
+		if (chdir(home_dir))
+		{
+			ft_dprintf(2, RED "minishell: cd : %s: No such file or directory\n"
+				WHITE, home_dir);
+			exit(1);
+		}
+		else
+		{
+			update_var(data, current_dir, "OLDPWD");
+			update_var(data, home_dir, "PWD");
+			return (0);
+		}
+	}
+	else
+	{
+		ft_dprintf(2, RED "minishell : cd: HOME not set\n" WHITE);
+		exit(1);
+	}
+}
+
 int	cd(t_data *data)
 {
 	char	*str;
@@ -64,5 +93,7 @@ int	cd(t_data *data)
 			return (0);
 		}
 	}
+	else
+		empty_cd(data);
 	return (0);
 }
